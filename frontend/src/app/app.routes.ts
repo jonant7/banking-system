e56ecda4a7face413a3cb3487c7d1bafc7517dd6
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2026-01-27 13:53:13 -0500

    feat(shared): add layout components structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainLayout} from './shared/components/layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'customers',
+        pathMatch: 'full'
+      },
+    ]
+  }
+];
