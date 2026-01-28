888467ce48ff27869cc2ef3825aa146b74cea661
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2026-01-28 03:47:51 -0500

    feat(customers): add components page routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import {MainLayout} from './shared/components/layout/main-layout/main-layout';
+import {MainLayout} from '@shared/components/layout/main-layout/main-layout';
 
 export const routes: Routes = [
   {
@@ -11,6 +11,27 @@ export const routes: Routes = [
         redirectTo: 'customers',
         pathMatch: 'full'
       },
+      {
+        path: 'customers',
+        loadComponent: () =>
+          import('./core/features/customers/pages/customer-list/customer-list.page').then(
+            (m) => m.CustomerListPage
+          )
+      },
+      {
+        path: 'customers/new',
+        loadComponent: () =>
+          import('./core/features/customers/pages/customer-form/customer-form.page').then(
+            (m) => m.CustomerFormPage
+          )
+      },
+      {
+        path: 'customers/:id/edit',
+        loadComponent: () =>
+          import('./core/features/customers/pages/customer-form/customer-form.page').then(
+            (m) => m.CustomerFormPage
+          )
+      }
     ]
   }
 ];
