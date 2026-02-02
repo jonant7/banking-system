57f9179b62322dceb81aeb31c39cfdb6120ae919
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2026-02-02 07:37:46 -0500

    feat(transactions): add transaction list component

---
+++ frontend/src/app/core/features/transactions/pages/transaction-list/transaction-list.page.spec.ts
@@ -0,0 +1,23 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {TransactionListPage} from './transaction-list.page';
+
+describe('TransactionListPage', () => {
+  let component: TransactionListPage;
+  let fixture: ComponentFixture<TransactionListPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TransactionListPage]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TransactionListPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
